4921f30cf62c2500ba046be38feb9fd1b778754d
Enda Figueroa <efigueroa@Zipcoders-MacBook-Pro-2.local>
2024-10-23 07:53:42 -0400

    Finished

--- object1/src/main/java/object1/Main.java
+++ object1/src/main/java/object1/Main.java
@@ -8,3 +8,10 @@ public class Main {
         Logger logger=Logger.getLogger(Main.class.getName());
         logger.info("This is a module-using Hello World!");    }
 }
+Person enda = new Person("Enda",38,170);
+Person abe = new Person("Abe",37,182);
+Person josh = new Person("Josh",35,178);
+system.out.println(enda.height);
+Chair comfy = new Chair("Silk","Green",4);
+Car reliable = new Car("Toyota","Yaris",2011,120000);
+Dog sandy = new Dog("Sandy",10,"Mutt");
